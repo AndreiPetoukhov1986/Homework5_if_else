6d158c229c73af2f5154d2daad74c7d0499f38b8
AndreiPetoukhov1986 <oziglena@mail.ru>
2022-12-27 16:27:59 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1 ( );
+        task2 ( );
+        task3 ( );
+        task4 ( );
+        task5 ( );
+        task6 ( );
+        task7 ( );
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int age = 17;
+        if (age>=18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
+    }
+    public static void task2 () {
+        System.out.println("Задача 2");
+        int temperatyre = 10;
+        if (temperatyre>=5) {
+            System.out.println("На улице " + temperatyre + " градусов, можно идти без шапки");
+        } else {
+            System.out.println("На улице " + temperatyre + " градусов, нужно надеть шапку");
+        }
+
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+        int speed = 59;
+        if (speed<=60) {
+            System.out.println("Если скорость " + speed + ", то можно ездить спокойно");
+        } else {
+            System.out.println("Если скорость " + speed + ", то придется заплатить штраф");
+        }
+    }
+    public static void task4 () {
+        System.out.println("Задача 4");
+
+    }
+    public static void task5 () {
+        System.out.println("Задача 5");
+    }
+    public static void task6 () {
+        System.out.println("Задача 6");
+    }
+    public static void task7 () {
+        System.out.println("Задача 7");
     }
 }
